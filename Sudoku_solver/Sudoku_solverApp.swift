f3c69f41438966cc51e8db32912ef2b99e1701c1
Menno Konijn <menno.konijn@usmedia.nl>
2024-01-31 15:18:28 +0100

    Initial Commit

---
+++ Sudoku_solver/Sudoku_solverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sudoku_solverApp.swift
+//  Sudoku_solver
+//
+//  Created by Menno Konijn on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sudoku_solverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
